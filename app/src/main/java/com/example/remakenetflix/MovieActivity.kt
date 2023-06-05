b4c236f963780a2ed56007dabbefa1f7f9eee450
Jhonatan Neves <nevesjn2@gmail.com>
2023-06-05 20:25:49 -0300

    API-URL

--- app/src/main/java/com/example/remakenetflix/MovieActivity.kt
+++ app/src/main/java/com/example/remakenetflix/MovieActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
@@ -17,28 +19,33 @@ import com.example.remakenetflix.model.MovieDetail
 import com.example.remakenetflix.util.MovieTask
 
 class MovieActivity : AppCompatActivity(), MovieTask.Callback {
+
+    private lateinit var txtTitle: TextView
+    private lateinit var txtDesc: TextView
+    private lateinit var txtCast: TextView
+    private lateinit var adapter: MovieAdapter
+    private lateinit var progress: ProgressBar
+    private val movies = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
-        val txtTitle: TextView = findViewById(R.id.movie_txt_title)
-        val txtDesc: TextView = findViewById(R.id.movie_txt_desc)
-        val txtCast: TextView = findViewById(R.id.movie_txt_cast)
-        val rv : RecyclerView = findViewById(R.id.movie_rv_similar)
+        txtTitle = findViewById(R.id.movie_txt_title)
+        txtDesc = findViewById(R.id.movie_txt_desc)
+        txtCast = findViewById(R.id.movie_txt_cast)
+        progress = findViewById(R.id.movie_progress)
+
+        val rv: RecyclerView  = findViewById(R.id.movie_rv_similar)
 
         val id = intent?.getIntExtra("id", 0) ?: throw IllegalStateException("ID not found!")
         val url = "https://api.tiagoaguiar.co/netflixapp/movie/$id?apiKey=19a4b35f-adde-49ee-bb71-d03dde25fd75"
 
         MovieTask(this).execute(url)
 
-        txtTitle.text = "Velozes & Furiosos 10 (2023)"
-        txtDesc.text = "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever."
-        txtCast.text = getString(R.string.cast, "Vin Diesel (Dominic 'Dom' Toretto), Michelle Rodrigues (Letty Ortiz), Tyrese Gibson (Roman Pearce), Ludacris (Tej Parker), John Cena (Jakob Toretto), Nathalie Emmanuel (Ramsey), Jordana Brewster(Mia Toretto), Sung Kang (Han Lue), Jason Momoa (Dante Reyes)")
-
-        val movies = mutableListOf<Movie>()
-
+        adapter = MovieAdapter(movies, R.layout.movie_item_similar)
         rv.layoutManager = GridLayoutManager(this, 3)
-        rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
+        rv.adapter = adapter
 
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
@@ -55,15 +62,24 @@ class MovieActivity : AppCompatActivity(), MovieTask.Callback {
     }
 
     override fun onPreExecute() {
-
+        progress.visibility = View.VISIBLE
     }
 
     override fun onFailure(message: String) {
+        progress.visibility = View.GONE
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
     override fun onResult(movieDetail: MovieDetail) {
-        Log.i("Teste", movieDetail.toString())
+        progress.visibility = View.GONE
+
+        txtTitle.text = movieDetail.movie.title
+            txtDesc.text = movieDetail.movie.desc
+            txtCast.text = getString(R.string.cast, movieDetail.movie.cast )
+
+        movies.clear()
+        movies.addAll(movieDetail.similars)
+        adapter.notifyDataSetChanged()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
